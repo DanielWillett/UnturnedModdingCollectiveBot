bbbd33f31a5c629a494ad7ba8cf263f3a07d780a
Daniel Willett <dannywill8@gmail.com>
2024-05-14 00:18:47 -0400

    Initial commit.

---
+++ UnturnedModdingCollective/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+HostApplicationBuilder hostBuilder = Host.CreateApplicationBuilder(args);
+
+hostBuilder.Services.AddSerilog((_, configuration) => configuration
+        .ReadFrom.Configuration(hostBuilder.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+    );
+
+IHost host = hostBuilder.Build();
+
+await host.RunAsync();
